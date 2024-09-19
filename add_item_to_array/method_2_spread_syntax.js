fc8856dfffe05d977f0b31e411c758483484832f
pippal5536 <pippal5536@gmail.com>
2024-09-19 05:53:33 +0100

    method 2 to add an item to an array

---
+++ add_item_to_array/method_2_spread_syntax.js
@@ -0,0 +1,10 @@
+/**
+ * Adds an item to an array using the spread syntax.
+ * @param {(string|number)[]} arr - The original array to which the item will be added.
+ * @param {(string|number)} item - The item to add to the array.
+ * @returns {(string|number)[]} - Returns a new array with the item added.
+ */
+const addItemInArray = (arr, item) => {
+  // Using the spread syntax.
+  return [...arr, item];
+};
